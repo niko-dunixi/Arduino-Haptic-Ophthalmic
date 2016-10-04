d0c1765d06aa2f428404d9be0319fad6e7765608
Paul Baker <paul.n.baker@utah.edu>
2016-10-03 19:33:15 -0600

    Shortening restart interval.

--- photon/debug.sh
+++ photon/debug.sh
@@ -2,6 +2,6 @@
 
 while [ true ]; do
   particle serial monitor | tee debug.log
-  echo "Serial connection down, restarting in 5s"
-  sleep 5s
+  echo "Serial connection down, restarting in 1s"
+  sleep 1s
 done
