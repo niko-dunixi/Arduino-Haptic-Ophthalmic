75985a98bd2859f0f0c6028822b3a14b8965d773
Paul Baker <paul.nelson.baker@gmail.com>
2016-10-19 23:07:43 -0600

    Added coments.

--- setup-linux.sh
+++ setup-linux.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-sudo adduser $USER vboxsf
+sudo adduser $USER vboxsf #For mounting vbox folders
 sudo apt-get update
 sudo apt-get install arduino arduino-core
-sudo usermod -aG dialout $USER
+sudo usermod -aG dialout $USER #Enable USB port for arduino
+sudo reboot
