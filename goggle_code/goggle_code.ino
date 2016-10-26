fcc46ead08fa3b6cd06ba2afd9118be5b32ca5be
TheMutedMan <joshuahardesty@gmail.com>
2016-10-26 17:28:25 -0600

    Still need to bring writeMotor into loop()
    I've gone through the goggle code, and just commented out what Paul has
    supplied. Attempting to use some of the example from NewPing library,
    but still need to account for outputting to the motor in the loop().

--- goggle_code/goggle_code.ino
+++ goggle_code/goggle_code.ino
@@ -6,46 +6,132 @@
  *  but the norm is to have a single code-base to edit so
  *  let's start centralizing our work)
  */
-const int farthestDistance = 25;
-const int closestDistance = 5;
+//const int farthestDistance = 25;
+//const int closestDistance = 5;
 
-typedef struct {
-  //int trigger;
-  //int echo;
-  NewPing sonar;
-  int motor;
-} SensorMotorNode;
+#define SONAR_NUM       3           // Number of sensors
+#define MAX_DISTANCE  200           // Max distance in cm
+#define MIN_DISTANCE   10           // Min distance in cm
+#define PING_INTERVAL  33           // Millisecond between pings. (3*33 = 99ms per ping cycle)
 
-SensorMotorNode nodes[] = {
-  { .sonar = NewPing(11, 12, farthestDistance), .motor = 3},
-  { .sonar = NewPing(10, 9, farthestDistance), .motor = 5 },
-  { .sonar = NewPing(8, 7, farthestDistance), .motor = 6 }
-  //{.sonar(11, 12, farthestDistance), .motor = 3 }
-  //{.trigger = 11, .echo = 12, .motor = 3},
-  //{.trigger = 10, .echo = 9, .motor = 5},
-  //{.trigger = 8, .echo = 7, .motor = 6}
+unsigned long pingTimer[SONAR_NUM]; // When each pings
+unsigned int cm[SONAR_NUM];         // Store ping distances
+uint8_t currentSensor = 0;          // Which sensor is active
+int motorSpeed = 0;
+int motorPin[3] = {
+  3,                                // Motor for sonar0
+  5,                                // Motor for sensor1
+  6                                 // Motor for sensor2
 };
 
-int node_count = 3;
+NewPing sonar[SONAR_NUM] = {        // Array of sensors
+  NewPing(11, 12, MAX_DISTANCE),
+  NewPing(10, 9, MAX_DISTANCE),
+  NewPing(8, 7, MAX_DISTANCE)
+};
+
+//typedef struct {
+//  //int trigger;
+//  //int echo;
+//  NewPing sonar;
+//  int motor;
+//} SensorMotorNode;
+//
+//SensorMotorNode nodes[] = {
+//  { .sonar = NewPing(11, 12, farthestDistance), .motor = 3},
+////  { .sonar = NewPing(10, 9, farthestDistance), .motor = 5 },
+////  { .sonar = NewPing(8, 7, farthestDistance), .motor = 6 }
+//  //{.sonar(11, 12, farthestDistance), .motor = 3 }
+//  //{.trigger = 11, .echo = 12, .motor = 3},
+//  //{.trigger = 10, .echo = 9, .motor = 5},
+//  //{.trigger = 8, .echo = 7, .motor = 6}
+//};
+//
+//int node_count = 3;
 
 void setup() {
-  for(int i = 0; i < node_count; ++i) {
-    //pinMode(nodes[i].trigger, OUTPUT);
-    //pinMode(nodes[i].echo, INPUT);
+  // Serial Port begin
+  Serial.begin (9600);
+  pingTimer[0] = millis() + 75; // First ping start in ms.
+  for (uint8_t i = 1; i < SONAR_NUM; i++)
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+
+  // Define motor pins
+//  pinMode (motorPin[0], OUTPUT);
+//  pinMode (motorPin[1], OUTPUT);
+//  pinMode (motorPin[2], OUTPUT);
+
+  for (int i = 0; i < sizeof(motorPin); i++) {
+    pinMode (motorPin[i], OUTPUT);
   }
+    
+//  for(int i = 0; i < node_count; ++i) {
+//    //pinMode(nodes[i].trigger, OUTPUT);
+//    //pinMode(nodes[i].echo, INPUT);
+//  }
 }
 
 void loop() {
-  for (int i = 0; i < node_count; ++i) {
-    int median = nodes[i].sonar.ping_median();
-    // Per docs, median is in microseconds and has to be converted to cm
-    int cm = nodes[i].sonar.convert_cm(median);
-    // Now we need to hit-up the motor
-    int motorSpeed = map(cm, closestDistance, farthestDistance, 200, 10);
-    if (cm < farthestDistance){
-      analogWrite(nodes[i].motor, motorSpeed);
-    } else {
-      analogWrite(nodes[i].motor, 0);
-    } 
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    if (millis() >= pingTimer[i]) {
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;
+      if (i == 0 && currentSensor == SONAR_NUM - 1)
+        oneSensorCycle(); // Do something with results.
+      sonar[currentSensor].timer_stop();
+      currentSensor = i;
+      cm[currentSensor] = 0;
+      sonar[currentSensor].ping_timer(echoCheck);
+    }
   }
-}
+//  delay(50);
+//  int uS = sonar.ping();
+//  for (int i = 0; i < node_count; ++i) {
+//    int median = nodes[i].sonar.ping_median();
+//    Serial.print("median: ");
+//    Serial.print(median);
+//    // Per docs, median is in microseconds and has to be converted to cm
+//    int cm = nodes[i].sonar.convert_cm(median);
+//    // Now we need to hit-up the motor
+//    int motorSpeed = map(cm, closestDistance, farthestDistance, 200, 10);
+//    Serial.print(" motorSpeed: ");
+//    Serial.println(motorSpeed);
+//    //Serial.print(cm);
+//    if (cm < farthestDistance){
+//      analogWrite(nodes[i].motor, motorSpeed);
+////      Serial.print(nodes[i].motor, motorSpeed);
+////      Serial.println() ;
+//    } else {
+//      analogWrite(nodes[i].motor, 0);
+//    } 
+//  }// end of for loop
+
+}//end of loop()
+
+void echoCheck() { // If ping echo, set distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}// end of echoCheck()
+ 
+void oneSensorCycle() { // Do something with the results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]);
+    Serial.print("cm ");
+  }
+  Serial.println();
+}// end of oneSensorCycle()
+
+void writeMotor(long dist, int motorToWrite){
+
+  if (dist < MAX_DISTANCE) {
+    motorSpeed = map(dist, MIN_DISTANCE, MAX_DISTANCE, 200, 10);
+
+    analogWrite(motorToWrite, motorSpeed);
+  }else{
+    analogWrite(motorToWrite, 0);
+  }
+}// end of writeMotor()
+
+
+
---
+++ newPing_Lib_Test/newPing_Lib_Test.ino
@@ -0,0 +1,104 @@
+#include <NewPing.h>
+ 
+//#define TRIGGER_PIN  11
+//#define ECHO_PIN     12
+//#define MAX_DISTANCE 200
+// 
+//NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+// 
+//void setup() {
+//  //Serial.begin(115200);
+//  Serial.begin(9600);
+//}
+// 
+//void loop() {
+//  delay(50);
+//  int uS = sonar.ping();
+//  Serial.print("Ping: ");
+//  Serial.print(uS / US_ROUNDTRIP_CM);
+//  Serial.println("cm");
+//}
+
+// ---------------------------------------------------------
+// This example code was used to successfully communicate
+// with 15 ultrasonic sensors. You can adjust the number of
+// sensors in your project by changing SONAR_NUM and the
+// number of NewPing objects in the "sonar" array. You also
+// need to change the pins for each sensor for the NewPing
+// objects. Each sensor is pinged at 33ms intervals. So, one
+// cycle of all sensors takes 495ms (33 * 15 = 495ms). The
+// results are sent to the "oneSensorCycle" function which
+// currently just displays the distance data. Your project
+// would normally process the sensor results in this
+// function (for example, decide if a robot needs to turn
+// and call the turn function). Keep in mind this example is
+// event-driven. Your complete sketch needs to be written so
+// there's no "delay" commands and the loop() cycles at
+// faster than a 33ms rate. If other processes take longer
+// than 33ms, you'll need to increase PING_INTERVAL so it
+// doesn't get behind.
+// ---------------------------------------------------------
+#include <NewPing.h>
+ 
+#define SONAR_NUM     15 // Number or sensors.
+#define MAX_DISTANCE 200 // Max distance in cm.
+#define PING_INTERVAL 33 // Milliseconds between pings.
+ 
+unsigned long pingTimer[SONAR_NUM]; // When each pings.
+unsigned int cm[SONAR_NUM]; // Store ping distances.
+uint8_t currentSensor = 0; // Which sensor is active.
+ 
+NewPing sonar[SONAR_NUM] = { // Sensor object array.
+  NewPing(41, 42, MAX_DISTANCE),
+  NewPing(43, 44, MAX_DISTANCE),
+  NewPing(45, 20, MAX_DISTANCE),
+  NewPing(21, 22, MAX_DISTANCE),
+  NewPing(23, 24, MAX_DISTANCE),
+  NewPing(25, 26, MAX_DISTANCE),
+  NewPing(27, 28, MAX_DISTANCE),
+  NewPing(29, 30, MAX_DISTANCE),
+  NewPing(31, 32, MAX_DISTANCE),
+  NewPing(34, 33, MAX_DISTANCE),
+  NewPing(35, 36, MAX_DISTANCE),
+  NewPing(37, 38, MAX_DISTANCE),
+  NewPing(39, 40, MAX_DISTANCE),
+  NewPing(50, 51, MAX_DISTANCE),
+  NewPing(52, 53, MAX_DISTANCE)
+};
+ 
+void setup() {
+  Serial.begin(115200);
+  pingTimer[0] = millis() + 75; // First ping start in ms.
+  for (uint8_t i = 1; i < SONAR_NUM; i++)
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+}
+ 
+void loop() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    if (millis() >= pingTimer[i]) {
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;
+      if (i == 0 && currentSensor == SONAR_NUM - 1)
+        oneSensorCycle(); // Do something with results.
+      sonar[currentSensor].timer_stop();
+      currentSensor = i;
+      cm[currentSensor] = 0;
+      sonar[currentSensor].ping_timer(echoCheck);
+    }
+  }
+  // The rest of your code would go here.
+}
+ 
+void echoCheck() { // If ping echo, set distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+ 
+void oneSensorCycle() { // Do something with the results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]);
+    Serial.print("cm ");
+  }
+  Serial.println();
+}
